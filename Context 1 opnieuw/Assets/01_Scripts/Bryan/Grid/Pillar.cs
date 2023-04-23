35f6d9828d136d5f4f9ac91116be38d3e385b1f4
Bryan <dinsbachbryan@gmail.com>
2023-04-23 22:11:11 +0200

    Add final artist skill to the game

---
+++ Context 1 opnieuw/Assets/01_Scripts/Bryan/Grid/Pillar.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using _01_Scripts.Bryan.ArtistSkill;
+using _01_Scripts.Bryan.DeveloperSkill;
+using UnityEngine;
+
+namespace _01_Scripts.Bryan.Grid
+{
+    public class Pillar : MonoBehaviour
+    {
+        [SerializeField] private MeshRenderer _meshRenderer;
+        [SerializeField] private MeshFilter _meshFilter;
+        
+        private MeshSwapModel _model;
+
+
+        public int Index;
+
+        private Action<int> _onPillarActivatedHandler;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            _onPillarActivatedHandler?.Invoke(Index);
+        }
+
+        public void AddPillarActivatedHandler(Action<int> onPillarActivatedHandler)
+        {
+            _onPillarActivatedHandler = onPillarActivatedHandler;
+        }
+        
+        //Set the model for this pillar
+        public void UpdateModel(MeshSwapModel model)
+        {
+            _model = model;
+        }
+
+        //Actual swap of the model
+        public void SwapModel()
+        {
+            _meshRenderer.material = _model.Material;
+            _meshFilter.mesh = _model.Mesh;
+        }
+    }
+}
