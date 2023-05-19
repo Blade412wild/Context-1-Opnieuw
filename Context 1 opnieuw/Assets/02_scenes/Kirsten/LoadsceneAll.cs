543136112b2fb7c6f365a9cacb79b14613df4097
KurryW <kwesterhuis@hotmail.com>
2023-05-19 15:02:30 +0200

    final fixes today

--- Context 1 opnieuw/Assets/02_scenes/Kirsten/LoadsceneAll.cs
+++ Context 1 opnieuw/Assets/02_scenes/Kirsten/LoadsceneAll.cs
@@ -22,6 +22,6 @@ public class LoadsceneAll : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("GOGOGOGO SCENE");
-        SceneManager.LoadScene("Eind scene");
+        SceneManager.LoadScene("Victory screen");
     }
 }
